eb64c8cec27892f823f86dcfdfdd616c000b4579
tiagovalenca <36921610+tiagovalenca@users.noreply.github.com>
2018-06-18 16:06:28 -0300

    Update fizzbizz.c
    Respondendo a questão

--- Q1/fizzbizz.c
+++ Q1/fizzbizz.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 /**
- * Implemente a função FizzBizz conforme definida abaixo. A mesma deve receber dois números inteiros não negativos (start e end) e imprimir a partir de start (incluído) até end (excluído) seguindo a mesma lógica do FizzBuzz.
- * 
- * Função: void fizzBizz(int start, int end)
- * Input: inteiros (start < end) determinando os números de início e fim da contagem.
- * Output: utilize a função printf da biblioteca stdio.h e escreva um número ou palavra por linha (\n):
- * 
- */
-void fizzBizz(int start, int end)
+* Implemente a função FizzBizz conforme definida abaixo. A mesma deve receber dois números inteiros não negativos (start e end) e imprimir a partir de start (incluído) até end (excluído) seguindo a mesma lógica do FizzBuzz.
+*
+* Função: void fizzBizz(int start, int end)
+* Input: inteiros (start < end) determinando os números de início e fim da contagem.
+* Output: utilize a função printf da biblioteca stdio.h e escreva um número ou palavra por linha (\n):
+*
+*/
+void fizzBuzz(int start, int end)
 {
-    printf("\nFizzBizz::START: %d, %d\n", start, end);
-    
-    // TODO: Your code here!
-    // Ex.: printf("Fizz\n");
-    
-    printf("\nFizzBizz::END\n");
+	printf("\nFizzBuzz::START: %d, %d\n", start, end);
+
+	int x;
+
+	for (x = start; x<end; x++)
+	{
+		if (x % 3 == 0) {
+			printf("Fizz");
+		}
+		if (x % 5 == 0) {
+			printf("Buzz");
+		}
+		else if ((x % 3 != 0) && (x % 5 != 0)) {
+			printf("%d", x);
+		}
+		printf("\n");
+	}
+
+	printf("\nFizzBuzz::END\n");
 }
 
 int main()
 {
-    printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: FizzBizz ###\n");
-    
-    fizzBizz(1, 6);
-    fizzBizz(1, 16);
-    fizzBizz(1, 100);
-    
-    return 0;
+	printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: FizzBuzz ###\n");
+
+	fizzBuzz(1, 6);
+	fizzBuzz(1, 16);
+	fizzBuzz(1, 100);
+
+	return 0;
 }
